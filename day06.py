acea695d6b2fcaa4e4a198cccebb7eb1e8a0c6aa
Tor Arve Stangeland <tor.arve.stangeland@gmail.com>
2022-12-06 06:10:31 +0100

    Cleanup day 6

--- day06.py
+++ day06.py
@@ -1,28 +1,22 @@
 from common import read_lines
 
 
-def sum_chars(line: str) -> dict[str,int]:
-    res = {}
-    for c in line:
-        if c in res:
-            res[c] += 1
-        else:
-            res[c] = 1
-    return res
+def find_marker(input: str, marker_size = 4) -> int:
+    def sum_chars(line: str) -> dict[str,int]:
+        res = {}
+        for c in line:
+            if c in res:
+                res[c] += 1
+            else:
+                res[c] = 1
+        return res
+    for i in range(0, len(input)-marker_size):
+        counts = sum_chars(input[i:i+marker_size])
+        if all([x==1 for x in counts.values()]):
+            return i+marker_size
 
-input = """mjqjpqmgbljsphdztnvjfqwrcgsmlb"""
+
 input = read_lines("input06.txt")[0]
 
-
-for i in range(0, len(input)-4):
-    counts = sum_chars(input[i:i+4])
-    if all([x==1 for x in counts.values()]):
-        print(i+4)
-        break
-
-
-for i in range(0, len(input)-14):
-    counts = sum_chars(input[i:i+14])
-    if all([x==1 for x in counts.values()]):
-        print(i+14)
-        break
+print(f"Answer part 1: {find_marker(input, 4)}")
+print(f"Answer part 1: {find_marker(input, 14)}")
