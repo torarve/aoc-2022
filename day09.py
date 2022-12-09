43939d01aa7e7a7ca02b63030ee479418e71110f
Tor Arve Stangeland <tor.arve.stangeland@gmail.com>
2022-12-09 06:33:07 +0100

    Clean up day 9.

--- day09.py
+++ day09.py
@@ -1,22 +1,24 @@
 from common import read_lines
 
-
-steps="""R 4
-U 4
-L 3
-D 1
-R 4
-D 1
-L 5
-R 2""".split("\n")
-
-steps = read_lines("input09.txt")
-
 def parse(line: str) -> tuple[int, str]:
+    """Parse single line"""
     tmp = line.split(" ")
     return tmp[0], int(tmp[1])
 
+def move_head(head: tuple[int, int], direction: str) -> tuple[int,int]:
+    """Move the first knot one step in the given direction."""
+    x, y = head
+    if direction == "U":
+        return (x, y+1)
+    elif direction == "D":
+        return (x, y-1)
+    elif direction == "L":
+        return (x-1, y)
+    elif direction == "R":
+        return (x+1, y)
+
 def update_tail(head: tuple[int, int], tail: tuple[int, int]) -> tuple[int, int]:
+    """Update position of knot based on previous knot"""
     hx, hy = head
     tx, ty = tail
     if hx > tx + 1:
@@ -46,53 +48,21 @@ def update_tail(head: tuple[int, int], tail: tuple[int, int]) -> tuple[int, int]
 
     return tx, ty
 
-def simulate_part1(steps: list[str]):
-    head = (0,0)
-    tail = (0,0)
+def simulate(steps: list[str], size: int):
+    knots = [(0,0)] * size
     tail_positions = set()
     for line in steps:
         direction, steps = parse(line)
         for i in range(1, steps+1):
-            x, y = head
-            if direction == "U":
-                head = (x, y+1)
-            elif direction == "D":
-                head = (x, y-1)
-            elif direction == "L":
-                head = (x-1, y)
-            elif direction == "R":
-                head = (x+1, y)
-
-            tail = update_tail(head, tail)
-            tail_positions.add(tail)
-
-    return len(tail_positions)
-
-
-def simulate_part2(steps: list[str]):
-    head = (0,0)
-    tails: list[tuple[int, int]] = [(0,0)] * 9
-    tail_positions = set()
-    for line in steps:
-        direction, steps = parse(line)
-        for i in range(1, steps+1):
-            x, y = head
-            if direction == "U":
-                head = (x, y+1)
-            elif direction == "D":
-                head = (x, y-1)
-            elif direction == "L":
-                head = (x-1, y)
-            elif direction == "R":
-                head = (x+1, y)
-
-            tails[0] = update_tail(head, tails[0])
-            for i in range(0, 8):
-                tails[i+1] = update_tail(tails[i], tails[i+1])
+            knots[0] = move_head(knots[0], direction)
+            for i in range(1, size):
+                knots[i] = update_tail(knots[i-1], knots[i])
             
-            tail_positions.add(tails[-1])
+            tail_positions.add(knots[-1])
 
     return len(tail_positions)
 
-print(simulate_part1(steps))
-print(simulate_part2(steps))
+
+steps = read_lines("input09.txt")
+print(f"Answer to part 1: {simulate(steps, 2)}")
+print(f"Answer to part 2: {simulate(steps, 10)}")
